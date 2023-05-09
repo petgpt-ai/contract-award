96be22ab81b812f2d595ab2d1ac4dcc8d860d1a4
d-oper <dopr@gmail.com>
2023-05-09 16:13:28 +0800

    add bin and abi

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-etherscan");
-require('@symblox/hardhat-abi-gen');
+require("@solidstate/hardhat-bytecode-exporter");
+require('hardhat-abi-exporter');
 require('@openzeppelin/hardhat-upgrades');
 const {
   TASK_COMPILE,
@@ -26,11 +27,11 @@ module.exports = {
     },
     goerli: {
       url: "https://goerli.infura.io/v3/a8bd8f6354ff4900b1f319a7e2c737b9",
-      accounts: [""]
+      accounts: ["6e3a4288130c72b420c4984034f9a7f343a985e3acd1114f0fc737af336fda3b"]
     },
     sepolia:{
       url: "https://sepolia.infura.io/v3/a8bd8f6354ff4900b1f319a7e2c737b9",
-      accounts: [""]
+      accounts: ["6e3a4288130c72b420c4984034f9a7f343a985e3acd1114f0fc737af336fda3b"]
     }
 
   },
